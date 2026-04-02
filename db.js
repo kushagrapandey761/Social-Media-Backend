0ea7043708cc225ed14b4f2464ed213693284c41
Kushagra Pandey <kushagra.pandey@kanlastech.com>
2026-04-02 21:57:53 +0530

    mongo changes

--- db.js
+++ db.js
@@ -6,6 +6,11 @@ const Follow = require("./schemas/followSchema");
 const Comment = require("./schemas/commentSchema");
 const Message = require("./schemas/messageSchema")
 
-mongoose.connect("mongodb://localhost:27017/test");
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch((err) => console.error("Error connecting to MongoDB:", err));
 
 module.exports = { User, Post, Follow, Comment, Message };
